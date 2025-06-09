7df5c58b48f770b33b3daa1d1530a4e8de2d2331
German Zakharov <german.zakharov12@gmail.com>
2025-06-08 21:16:43 -0400

    Update ImageCompressor.py

--- Image Compressor/ImageCompressor.py
+++ Image Compressor/ImageCompressor.py
@@ -11,32 +11,22 @@ class ImageCompressor(object):
         
     def BlockAveraging(self,image_filename):
         """Method for image compression based on Block Averaging Algorithm"""
-        block_side_length = 4
+        N = 3
         pixel_matrix = cv2.imread(self.original_path+image_filename)
         l_lim,w_lim,c_lim = list(pixel_matrix.shape)
-        pixel_matrix_split_to_channels = cv2.split(pixel_matrix)
-        
-        channels_compressed = [[],[],[]]
-
-        #Divide picture into 4 by 4 pixel blocks
-        for channel in range(len(channels_compressed)):
+        pixel_matrix = cv2.split(pixel_matrix)
+        #Divide picture into equal blocks (N by N size)
+        for channel in range(len(pixel_matrix)):
             block_y_coord,block_x_coord = [0,0]
-            while block_y_coord + block_side_length <= l_lim:
+            while block_y_coord + N <= l_lim:
                 block_x_coord = 0
-                row = []
-                while block_x_coord + block_side_length <= w_lim:
-                    #Take the average color of each block.
-                    #Create the pixel matrix, with the entry values, according to the relative block location.
-                    row.append(round(np.mean(pixel_matrix_split_to_channels[channel][block_y_coord:block_y_coord + block_side_length,
-                                                                                     block_x_coord:block_x_coord + block_side_length]
-                                             )
-                                     )
-                               )
-                    block_x_coord+=block_side_length
-                channels_compressed[channel].append(row)
-                block_y_coord+=block_side_length
+                while block_x_coord + N <= w_lim:
+                    #Take the average color value of each block and substitute values around the block by the average.
+                    pixel_matrix[channel][block_y_coord:block_y_coord + N,block_x_coord:block_x_coord + N] = round(np.mean(pixel_matrix[channel][block_y_coord:block_y_coord + N,block_x_coord:block_x_coord + N]))
+                    block_x_coord+=N
+                block_y_coord+=N
 
-        merged = cv2.merge(np.array(channels_compressed))
+        merged = cv2.merge(np.array(pixel_matrix))
         #Save it
         cv2.imwrite(self.compression_path+"\\"+image_filename,merged)
         
@@ -44,17 +34,15 @@ class ImageCompressor(object):
         self.original_path+="\\"
         #Getting the list of filenames only with .jpg,.png and .jpeg extensions
         original_files = [filename for filename in os.listdir(self.original_path) if filename.split('.')[len(filename.split('.'))-1] in ['jpg','png','jpeg']]
-        #If a path for compressed pictures is not mentioned, create Compression_Folder inside of the path with the original pictures
+        #If a path for saving compressed pictures were not specified, create nested directory inside of the directory with original files.
         if self.compression_path is None:
             self.compression_path = self.original_path+"Compression_Folder"
-        #Сreate folder for compressioned pictures, if it doesn't exist
         if not os.path.exists(self.compression_path):
             os.mkdir(self.compression_path)
         #Compressing pictures
         for filename in original_files:
-            if filename not in os.listdir(self.compression_path + "\\"):
-                self.BlockAveraging(filename)
+            self.BlockAveraging(filename)
         return 0
 
 if __name__=='__main__':
-    ImageCompressor("original pictures pathway","pathway for compressed pictures (optional)").main()
+    ImageCompressor("path with pictures to be compressed. Use '\\' instead of '\'","path for compressed pictures (optional). Use '\\' instead of '\'").main()
